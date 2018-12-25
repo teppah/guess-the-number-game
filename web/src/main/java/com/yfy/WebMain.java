3661c9b6182f2e1bfc24e60ae1ba9a651d9ea417
teppah <yifengyang01@gmail.com>
2018-12-25 16:24:17 -0500

    removed unnecessary imports

--- web/src/main/java/com/yfy/WebMain.java
+++ web/src/main/java/com/yfy/WebMain.java
@@ -4,6 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.AbstractClientHttpResponse;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootApplication
 @Slf4j
