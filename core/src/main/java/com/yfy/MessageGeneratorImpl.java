454b21ed30d3753861255a3eac74e2787c29adea
teppah <yifengyang01@gmail.com>
2018-12-25 16:24:35 -0500

    i18n the core game logic messages

--- core/src/main/java/com/yfy/MessageGeneratorImpl.java
+++ core/src/main/java/com/yfy/MessageGeneratorImpl.java
@@ -1,6 +1,11 @@
 package com.yfy;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -9,15 +14,25 @@ import javax.annotation.PreDestroy;
 @Slf4j
 @Component
 public class MessageGeneratorImpl implements MessageGenerator {
-
+    // == private constants ==
+    private static final String MAIN_MESSAGE = "game.main.message";
+    private static final String WIN = "game.win";
+    private static final String LOSE = "game.lose";
+    private static final String INVALID_RANGE = "game.invalid.range";
+    private static final String FIRST_GUESS = "game.first.guess";
+    private static final String HIGHER = "game.higher";
+    private static final String LOWER = "game.lower";
+    private static final String REMAINING = "game.remaining";
     // == fields ==
     private final Game game;
+    private final MessageSource messageSource;
+
 
     // == constructor ==
-
-
-    public MessageGeneratorImpl(Game game) {
+//    @Autowired
+    public MessageGeneratorImpl(Game game, MessageSource source) {
         this.game = game;
+        this.messageSource = source;
     }
 
     // == init ==
@@ -30,35 +45,34 @@ public class MessageGeneratorImpl implements MessageGenerator {
     public void destroy() {
         log.info("game destroyed, = {}", game);
     }
+
     // == public methods ==
     @Override
     public String getMainMessage() {
-        return "Number is between " +
-                game.getSmallest() +
-                " and " +
-                game.getBiggest() +
-                ". Can you guess it?";
+        return getMessage(MAIN_MESSAGE, game.getSmallest(), game.getBiggest());
     }
 
     @Override
     public String getResultMessage() {
-
-        if(game.isGameWon()) {
-            return "You guessed it! The number was " + game.getNumber();
-        } else if(game.isGameLost()) {
-            return "You lost. The number was " + game.getNumber();
-        } else if(!game.isValidNumberRange()) {
-            return "Invalid number range!";
-        } else if(game.getRemainingGuesses() == game.getGuessCount()) {
-            return "What is your first guess?";
+        if (game.isGameWon()) {
+            return getMessage(WIN, game.getNumber());
+        } else if (game.isGameLost()) {
+            return getMessage(LOSE, game.getNumber());
+        } else if (!game.isValidNumberRange()) {
+            return getMessage(INVALID_RANGE);
+        } else if (game.getRemainingGuesses() == game.getGuessCount()) {
+            return getMessage(FIRST_GUESS);
         } else {
-            String direction = "Lower";
-
-            if(game.getGuess() < game.getNumber()) {
-                direction = "Higher";
+            String direction = getMessage(LOWER);
+            if (game.getGuess() < game.getNumber()) {
+                direction = getMessage(HIGHER);
             }
-
-            return direction + "! You have " + game.getRemainingGuesses() + " guesses left";
+            return getMessage(REMAINING, direction, game.getRemainingGuesses());
         }
+    }
+
+    // == private methods ==
+    private String getMessage(String code, Object... args) {
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
     }
 }
